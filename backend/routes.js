36e29ba62f0174590395b75c52e37e16dcaffa66
haithamelmengad <haitham.elmengad@gmail.com>
2018-03-22 19:20:38 -0700

    Add comments, improve html text and UI

--- backend/routes.js
+++ backend/routes.js
@@ -1,3 +1,8 @@
+// This file uses express middleware as a server, path to join directory path to relative path
+// bodyParser to receive parsed request data request.body object
+// fs to read external data (from the data folder)
+// csv parser to parse the external data 
+
 import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
@@ -6,54 +11,53 @@ import csv from "csv-parser";
 
 const router = express();
 
+// Join directory path to public folder to set static path to this file
+// Use body parser and defines data format (url encoded) for http protocol
 router.use(express.static(path.join(__dirname, "../public")));
 router.use(bodyParser.urlencoded({
 	extended: true
 }));
 
-// Code to calculate area based on X,Y coordinates in a plane
-// convert lat/lngs from degrees to distances
-// const calculateArea = (arrayCoords) => {
-// 	const earthRadius = ;
-
-// 	let area = 0;
-// 	let lastVertex = arrayCoords.length-1;
-
-// 	for (let i = 0; i < arrayCoords.length; i++) {
-// 		area = (arrayCoords[i].lat+arrayCoords[lastVertex].lat) + (arrayCoords[i].lng-arrayCoords[lastVertex].lng);
-// 		lastVertex = i;
-// 	}
-// 	return area/2;
-// };
-
-// (-124.848974, 24.396308) - (-66.885444, 49.384358)
-
+// Load index.hmtl at url endpoint "/" from listening port
 router.get("/", (req, res) => {
 	res.sendFile("index.html");
 });
 
+// Post end point matching the submit button in the frontend
 router.post("/calculate", (req, res) =>{
+	// Store coordinated of first vertex of polygon
 	const firstVertex = req.body.polygonCoordinates[0];
 	let directRadiation;
 	let diffuseRadiation;
+	
+	// Load data from NASA dataset Annual direct radiation (kWh/(m^2*day) at given lat/lng
+	// Source: https://eosweb.larc.nasa.gov/sse/global/text/direct_radiation
 	fs.createReadStream("./data/direct_radiation.csv")
 		.pipe(csv({separator: " ",
 			headers: ["Lat", "Lon", "Ann"]}))
 		.on("data", (data) => { 
+			// Check if current polygon is within less than 0.5 degrees of latitude and longitude 
+			// from current data chunk. If so, store the value of the annual radiation as direct radiation
 			if((Math.abs(Math.abs(data.Lat)-Math.abs(firstVertex.lat)) < 0.5 && Math.abs(Math.abs(data.Lon)-Math.abs(firstVertex.lng))<0.5)){
 				directRadiation = parseFloat(data.Ann);
 			}
 		})
 		.on("end", () => {
+			// Load data from NASA dataset Annual diffuse radiation (kWh/(m^2*day) at given lat/lng
+			// Source: https://eosweb.larc.nasa.gov/sse/global/text/diffuse_radiation
 			fs.createReadStream("./data/diffuse_radiation.csv")
 				.pipe(csv({separator: " ",
 					headers: ["Lat", "Lon", "Ann"]}))
 				.on("data", (data) => { 
+					// Check if current polygon is within less than 0.5 degrees of latitude and longitude 
+					// from current data chunk. If so, store the value of the annual radiation as diffuse radiation 
 					if((Math.abs(Math.abs(data.Lat)-Math.abs(firstVertex.lat)) < 0.5 && Math.abs(Math.abs(data.Lon)-Math.abs(firstVertex.lng))<0.5)){
 						diffuseRadiation = parseFloat(data.Ann);
 					}
 				})	
 				.on("end", () => {
+					// Ladies and gentlemen, welcome to callback hell. Note that we could use another tool such as axios
+					// To promisify and make this look better, but with only two function, I thought it wasn't necessary
 					let polygonCoords = [];
 					let polygonArea = 0;
 					req.body.polygonCoordinates.forEach((item) => {
@@ -63,20 +67,29 @@ router.post("/calculate", (req, res) =>{
 							polygonCoords.push(item);
 						}
 					});
+					// Calculate irradiance using sum of direct and diffuse radiation
+					// multiply by 41.6666 to convert (kWh/(m^2*day) to W/m2
 					const estimatedIrradiance = 41.6666 * (directRadiation + diffuseRadiation);
+					// Assumptions: panel efficiency (18%), panel size (40*60 inches or 1.6 m^2), spacing (0.1m^2)
 					const efficiency = 0.18;
 					const panelSize = 1.6;
-					const spacingArea= 2;
-					const panelArea = Math.floor(polygonArea/panelSize)-spacingArea;
+					const spacingArea= 0.1;
 					const solarIrradiance = 1000; 
+					// Calculate total area of solar panels
+					const panelArea = Math.floor(polygonArea/panelSize)-Math.floor(polygonArea/panelSize)*spacingArea;
+
+					// Calculate impact of tilt angle if the user did provide one
 					let orientationImpact = 1;
 					if(req.body.tiltangle){
 						orientationImpact = Math.cos(req.body.tiltangle * Math.PI/180);
 					}
-
+					// Calculate nominal power using Std solar irradiance : 1000 W/m2
 					const nominalPow = Math.floor(efficiency * panelArea * solarIrradiance * orientationImpact);
+					// Calculate real power output using estimated irradiance
 					const realPow = Math.floor(efficiency * panelArea * estimatedIrradiance * orientationImpact);
-					res.send({nominalPower : nominalPow, realPower : realPow });
+
+					// Send results to front end
+					res.send({estimatedIrradiance: estimatedIrradiance, nominalPower : nominalPow, realPower : realPow });
 				});
 		});
 });
--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 // Use "var" to hoist initMap globally 
-// So it can be used as a callback after google maps API is loaded
+// So it can be used as a callback after google maps API is loaded in the html file
+// This code uses jQuery (script at the top of the HTML page, and google maps API - script at the end of the page)
 var initMap = () => {
 	let polygonCoordinates = [];
 	// Initialize google maps map object centered on Somerville, MA
@@ -14,15 +15,19 @@ var initMap = () => {
 	let autocomplete = new google.maps.places.Autocomplete(input, { componentRestrictions: {country: "us"} });
 	map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
       
-      
+	// Listen to event and fire a callback upon to search entries and 
 	google.maps.event.addListener(autocomplete, "place_changed", (event) => {
+		// Center the map on the inputted adress and zoom
 		let searchedAddress = autocomplete.getPlace();
 		map.setZoom(20);
 		map.setCenter(searchedAddress.geometry.location);
-		$("#section").append("<p>Great! Now you can approximate your solar installation using the draw tool on the top right corner of the map</p>");
+
+		// Guide user by adding HTML element to the side bar and describing the next step to be taken.
+		$("#section").append("<p>Great! Now you can approximate your solar installation using the drawing tool (top right).</p>");
 	});
 
-	// Create drawing GU
+	// Create a drawing GUI using google maps' built-in Drawing Manager class
+	// Definition
 	let drawingManager = new google.maps.drawing.DrawingManager({
 		drawingControl: true,
 		drawingControlOptions: {
@@ -38,13 +43,17 @@ var initMap = () => {
 			zIndex: 1
 		}
 	});
+	// Setup
 	drawingManager.setMap(map);
 
-	
+	// When the polygon is drawn execute callback
 	google.maps.event.addListener(drawingManager, "polygoncomplete", (event) => {
 		let latlngObjects = [];
 		const latlngCb = event.latLngs.b[0];             
 		const length = latlngCb.length;
+
+		// Store longitudes and latitudes of polygon vertices in a an array of objects
+		// and in another array of google maps LatLng objects (to be able to use google maps methods)
 		for( let i = 0; i < length ; i++) {
 			let lat = latlngCb.b[i].lat();
 			let lng = latlngCb.b[i].lng();
@@ -53,31 +62,42 @@ var initMap = () => {
 			polygonCoordinates.push(coords);
 			latlngObjects.push(latlngs);
 		}
+		// Compute area and store it in the array of polygon coordinates
 		polygonCoordinates.push({area: google.maps.geometry.spherical.computeArea(latlngObjects)});
-		// Convert array to data format supported by fetch API
+
+		// This code is not async, so the following will run after the array is populate with polygon data
+		// This is an HTML component to guide the user. It provides the option of including a tilt angle for the solar assessment
 		$("#section").append(`<div>
-		<p>
-		You can input your desired tilt angle of your panel if you wish.
-		If you don't know what we're talking about, just click submit and you're good to go!
-		</p><form action="/calculate" method="post">
-		<p>Tilt angle (degrees):</p><br>
-		<input type="text" id="tiltangle" name="tiltangle"><br>
-		<input id="calculate" type="submit" value="Submit">
-	  </form>
-	  </div>`);
-	  $("#calculate").on("click", (event) => {
+			<p>You can input your desired tilt angle of your panel if you wish.
+			If you don't know what we're talking about, just click submit and you're good to go!
+			</p>
+			<form action="/calculate" method="post">
+			<p>Tilt angle (degrees):</p><br>
+			<input type="text" id="tiltangle" name="tiltangle"><br>
+			<input id="calculate" type="submit" value="Submit">
+			</form>
+		  </div>`);
+		// Add a click listener to the submit button that intiates the solar power calculation
+		// by sending a post request to the backend
+		$("#calculate").on("click", (event) => {
 			event.preventDefault();
 			let tiltangle = $("#tiltangle").val();
-			let azimuth = $("#azimuth").val();
+			// Fire the ajax request to the /calculate endpoint in the backend
+			// Notice how the click listener is defined inside of the polygon complete listener
+			// This is because the ajax request is async and it would thus be firing arbitraly, before the
+			// polygon coordinate array is populated. I could use promises to do this differently, but with only
+			// one endpoint, I thought it was not necessary
 			$.ajax({
 				url:"/calculate",
 				type: "post",
 				data: {polygonCoordinates : polygonCoordinates,
-					tiltangle : tiltangle,
-					azimuth : azimuth} ,
+					tiltangle : tiltangle} ,
 				success: (response) => {
-					$("#section").append(`<p> Your solar installation is rated at " + response.nominalPower/1000 
-					+" kW of nominal power and  "+ response.realPower/1000 +" kW of real power output</p><a href="/">New Solar Search</a>`);
+					$("#section").append(`
+					<p> The estimated solar irradiance in that area is ` + response.estimatedIrradiance +` W/m^2 </p>
+					<p> Your solar installation is rated at  `+ response.nominalPower/1000+ `
+					 kW of nominal power and  `+ response.realPower/1000 +` kW of real power output </p>
+					 <a href="/">New Solar Search</a>`);
 				}
 			});
 		});
